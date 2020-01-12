e2a0fb8871124a1cdecd78695ff14e6544a8a4d2
sipho-kwesi <siphokwesi123@yahoo.com>
2020-01-12 23:02:20 +0000

    first commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,19 @@
-//create a function called computerPlay which will randomly return either 'Rock', 'Paper', or 'Scissors'
-//This function will be used in the game to make the computer's play.
-function computerPlay(x) {
+//create a function called computerPlay which will randomly return either 'Rock', 'Paper', or 'Scissors' 
+function computerPlay() {
     const hands = ['Rock', 'Paper', 'Scissors'];  
-    //use the Math.random function to generate a random number between 0 and 1 
-    //multiply it by arr.length to make sure the number is between 0 and array length
-    //use the math floor to get the index ranging from 0 - array length
+//use the Math.random function to generate a random number between 0 and 1 
+//multiply it by arr.length to make sure the number is between 0 and array length
     return hands[Math.floor(Math.random() * hands.length)];
 }
 
-//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - playerSelection and computerSelection
-//The function must return a string that declares the winner of the round
+/*Write a function that plays a single round of Rock Paper Scissors. 
+The function should take two parameters - playerSelection and computerSelection
+The function must return a string that declares the winner of the round */
 
 function round(playerSelection, computerSelection) {
-//Players make their selection
-//Store results
-        let results = ['You Win', 'You Lose', 'It\'s a draw'];
-        computerSelection = computerPlay();
-        playerSelection = prompt("Rock, Paper, or Scissors?");
+    let results = ['You Win', 'You Lose', 'It\'s a draw'];
+    computerSelection = computerPlay();
+    playerSelection = prompt("Rock, Paper, or Scissors?");
 //play a single round of the game
     if(playerSelection == computerSelection){
         console.log(results[2]);
@@ -34,3 +31,4 @@ function round(playerSelection, computerSelection) {
         console.log(`${results[1]} Rock, Smashes Scissors`);
     }
 }
+
