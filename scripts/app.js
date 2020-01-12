76489f719fe0ee2475bd5e752e13c3748e75c072
sipho-kwesi <siphokwesi123@yahoo.com>
2020-01-12 20:11:38 +0000

    written a function called round, that plays a single round of the game

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,32 @@ function computerPlay(x) {
     //use the Math.random function to generate a random number between 0 and 1 
     //multiply it by arr.length to make sure the number is between 0 and array length
     //use the math floor to get the index ranging from 0 - array length
-    return hands[Math.floor(Math.random() * hands.length)];
+    return hands[Math.floor(Math.random() * 3)];
 }
 
+//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - playerSelection and computerSelection
+//The function must return a string that declares the winner of the round
+
+function round(playerSelection, computerSelection) {
+//Players make their selection
+//Store results
+        let results = ['You Win', 'You Lose', 'It\'s a draw'];
+        computerSelection = computerPlay();
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+//play a single round of the game
+        if(playerSelection == computerSelection){
+        console.log(results[2]);
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors" ){
+        console.log(`${results[0]} Rock, Smashes Scissors!`)
+    } else if (playerSelction == "Rock" && computerSelection == "Paper"){
+        console.log(`${results[1]} Paper, Covers Rock!`);
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        console.log(`${results[0]} Paper, Covers Rock!`);
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        console.log(`${results[1]} Scissors, Cuts Paper!`);
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        console.log(`${results[0]} Scissors, Cuts Paper`);
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+        console.log(`${results[1]} Rock, Smashes Scissors`);
+    }
+}
