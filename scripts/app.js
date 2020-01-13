e5f196c11782ef03883ac17963e33ffb6d366543
Sipho Kwesi Tabiri <59542514+sipho-kwesi@users.noreply.github.com>
2020-01-13 12:28:01 +0000

    Updated app.js
    created a helper function to get user input for the game and made the round function case insensitive.

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,45 @@
-//create a function called computerPlay which will randomly return either 'Rock', 'Paper', or 'Scissors' 
+
+//create a function called computerPlay which will randomly return either 'Rock', 'Paper', or 'Scissors'
+//This function will be used in the game to make the computer's play.
 function computerPlay() {
-    const hands = ['Rock', 'Paper', 'Scissors'];  
-//use the Math.random function to generate a random number between 0 and 1 
-//multiply it by arr.length to make sure the number is between 0 and array length
+    const hands = ['rock', 'paper', 'scissors'];  
+    //use the Math.random function to generate a random number between 0 and 1 
+    //multiply it by arr.length to make sure the number is between 0 and array length
+    //use the math floor to get the index ranging from 0 - array length
     return hands[Math.floor(Math.random() * hands.length)];
 }
 
-/*Write a function that plays a single round of Rock Paper Scissors. 
-The function should take two parameters - playerSelection and computerSelection
-The function must return a string that declares the winner of the round */
+//write a helper function that makes your round function case insensitive 
 
+function getInput() {
+    let x = prompt("Rock, Paper, or Scissors?");
+    x = x.toLowerCase();
+    return x;
+}
+
+//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - playerSelection and computerSelection
+//The function must return a string that declares the winner of the round
 function round(playerSelection, computerSelection) {
-    let results = ['You Win', 'You Lose', 'It\'s a draw'];
-    computerSelection = computerPlay();
-    playerSelection = prompt("Rock, Paper, or Scissors?");
+//Players make their selection
+//Store results
+        let results = ['You Win', 'You Lose', 'It\'s a draw'];
+        computerSelection = computerPlay();
+        playerSelection = getInput();
+        
 //play a single round of the game
     if(playerSelection == computerSelection){
         console.log(results[2]);
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors" ){
+    } else if (playerSelection == "rock" && computerSelection == "scissors" ){
         console.log(`${results[0]} Rock, Smashes Scissors!`)
-    } else if (playerSelection == "Rock" && computerSelection == "Paper"){
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
         console.log(`${results[1]} Paper, Covers Rock!`);
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
         console.log(`${results[0]} Paper, Covers Rock!`);
-    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+    } else if (playerSelection == "paper" && computerSelection == "scissors"){
         console.log(`${results[1]} Scissors, Cuts Paper!`);
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+    } else if (playerSelection == "scissors" && computerSelection == "paper"){
         console.log(`${results[0]} Scissors, Cuts Paper`);
-    } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+    } else if (playerSelection == "scissors" && computerSelection == "rock"){
         console.log(`${results[1]} Rock, Smashes Scissors`);
     }
 }
-
