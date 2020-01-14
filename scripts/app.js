9b1cfd575ce46d324cce4c254b6e265869973343
Sipho Kwesi Tabiri <59542514+sipho-kwesi@users.noreply.github.com>
2020-01-14 11:30:16 +0000

    Update app.js
    code maintenance

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,8 @@
 
 //create a function called computerPlay which will randomly return either 'Rock', 'Paper', or 'Scissors'
-//This function will be used in the game to make the computer's play.
+
 function computerPlay() {
     const hands = ['rock', 'paper', 'scissors'];  
-    //use the Math.random function to generate a random number between 0 and 1 
-    //multiply it by arr.length to make sure the number is between 0 and array length
-    //use the math floor to get the index ranging from 0 - array length
     return hands[Math.floor(Math.random() * hands.length)];
 }
 
@@ -19,14 +16,11 @@ function getInput() {
 
 //Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - playerSelection and computerSelection
 //The function must return a string that declares the winner of the round
-function round(playerSelection, computerSelection) {
-//Players make their selection
-//Store results
-        let results = ['You Win', 'You Lose', 'It\'s a draw'];
-        computerSelection = computerPlay();
-        playerSelection = getInput();
-        
-//play a single round of the game
+
+function playRound(playerSelection, computerSelection) {
+    let results = ['You Win', 'You Lose', 'It\'s a draw'];     
+    
+//play a single round
     if(playerSelection == computerSelection){
         console.log(results[2]);
     } else if (playerSelection == "rock" && computerSelection == "scissors" ){
@@ -43,3 +37,7 @@ function round(playerSelection, computerSelection) {
         console.log(`${results[1]} Rock, Smashes Scissors`);
     }
 }
+
+computerSelection = computerPlay();
+playerSelection = getInput();
+console.log(playRound(playerSelection, computerSelection));
